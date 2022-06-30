8ce3e55f4424adddf5b0fa7ed4aeeea1b681a868
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2022-07-01 02:46:39 +0300

    test

--- test/test.js
+++ test/test.js
@@ -11,3 +11,62 @@ var swiper = new Swiper(".blog-slider", {
     clickable: true,
   },
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
+
+$(document).ready(function () {
+  "use strict";
+
+  var c,
+    currentScrollTop = 0,
+    navbar = $("nav");
+
+  $(window).scroll(function () {
+    var a = $(window).scrollTop();
+    var b = navbar.height();
+
+    currentScrollTop = a;
+
+    if (c < currentScrollTop && a > b + b) {
+      navbar.addClass("scrollUp");
+    } else if (c > currentScrollTop && !(a <= b)) {
+      navbar.removeClass("scrollUp");
+    }
+    c = currentScrollTop;
+
+    console.log(a);
+  });
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+$(document).ready(function () {
+  setInterval(function () {
+    $("#kupasen-ugakimev>span").addClass("blink_on");
+    setTimeout(function () {
+      $("#kupasen-ugakimev>span").removeClass("blink_on");
+    }, 1500);
+  }, 6000);
+});

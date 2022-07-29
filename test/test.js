4445b91681c5a304b7d05881e14c916a4d83532b
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2022-07-29 14:15:50 +0300

    кто мы тест

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".blog-slider", {
+let swiper = new Swiper(".blog-slider", {
   spaceBetween: 30,
   effect: "fade",
   loop: true,
@@ -62,3 +62,21 @@ window.addEventListener("scroll", () => {
 $(window).on("load", function () {
   $(".preloader").fadeOut().end().delay(500).fadeOut("slow");
 });
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
